a089dfb0c45c726006eb68b93660a6d32e22d4e2
Jonathan Fambro <fambrotekproductions@gmail.com>
2024-12-18 16:57:36 -0500

    Creating New Components

---
+++ myngapp/src/app/hello-world.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hello-world',
+  imports: [CommonModule],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css',
+})
+export class HelloWorldComponent {}
